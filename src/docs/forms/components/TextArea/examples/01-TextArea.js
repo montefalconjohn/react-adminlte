2966d4d37a527892d600d79b90e8b6d4603864d7
Maynard Montefalcon <33montefalconyoyon@gmail.com>
2019-08-05 13:42:13 +0800

    Added description in TextArea component

---
+++ src/docs/forms/components/TextArea/examples/01-TextArea.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {TextArea} from "doj-react-adminlte/Form";
+
+export default class TextArea01 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: ''
+        };
+    }
+    handleChange = (name, value) => {
+        this.setState({[name]: value});
+    };
+
+    render () {
+        return (
+            <div className="row">
+                <div className="col-xs-6">
+                    <TextArea name="keyword"
+                              label="Basic Text Area"
+                              value={this.state.keyword}
+                              onChange={this.handleChange}/>
+                </div>
+                <div className="col-xs-6">
+                    <TextArea label="Disabled TextArea with placeholder"
+                              onChange={this.handleChange}
+                              disabled
+                              gridClass="roundTextArea"
+                              maxLength={255}
+                              placeholder="TextArea with placeholder"/>
+                </div>
+            </div>
+        )
+    }
+}
